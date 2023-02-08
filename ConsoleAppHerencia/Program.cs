85a58928bcd691201fc52d845a333eedc8e5a1dc
Guido Amici <guido.amici95@gmail.com>
2023-02-08 01:20:27 -0300

    changes.

--- ConsoleAppHerencia/Program.cs
+++ ConsoleAppHerencia/Program.cs
@@ -8,18 +8,9 @@ static void Main(string[] args)
 {
 
     Animal objetoAnimal;
-    string lugar;
 
     objetoAnimal = null;
     objetoAnimal = new();
-    lugar = string.Empty;
 
     Test.Run(objetoAnimal);
-
-    Test.Show(objetoAnimal);
-
-    Console.WriteLine("Habia una vez en algun lugar de la tierra");
-    Console.WriteLine("Un " + objetoAnimal);
-    Console.WriteLine("Ya que este aninmal era un " + objetoAnimal.getTipo();
-    Console.WriteLine(objetoAnimal.getNombre() + "solo podía " + objetoAnimal.getMetodoDesplazamiento();
 }
